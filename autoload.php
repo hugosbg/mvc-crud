52d8d45e7851f1cbd2a8816753462ff6e391d2ab
Hugo Sóstenes <hugo.msn@msn.com>
2017-03-17 12:45:55 -0300

    remove doc ide

--- autoload.php
+++ autoload.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: macbook
- * Date: 18/02/17
- * Time: 15:09
- */
 
 spl_autoload_register(function($class) {
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
